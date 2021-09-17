da4c75e894429a490cfe4748286853dddf9e20f5
Rafiou Sitou <rafiousitou90@gmail.com>
2021-09-16 22:56:33 -0300

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # POO
+
 Java POO API Projects
---
+++ src/main/java/com/rafdev/prova/blog/api/ProvaBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafdev.prova.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaBlogApiApplication.class, args);
+    }
+
+}
