397332abb96653b20d0b34bfdc78787bd1472f7e
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2021-11-05 01:09:15 -0300

    Update Repository class to interface

--- src/main/java/com/rafdev/prova/blog/api/repository/RoleRepository.java
+++ src/main/java/com/rafdev/prova/blog/api/repository/RoleRepository.java
@@ -1,33 +1,11 @@
 package com.rafdev.prova.blog.api.repository;
 
-import com.rafdev.prova.blog.api.database.Database;
-import com.rafdev.prova.blog.api.entity.ERole;
 import com.rafdev.prova.blog.api.entity.Role;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    private final List<Role> roles;
-
-    public RoleRepository(Database database) {
-        this.roles = database.getRoles();
-    }
-
-    public List<Role> findAll() {
-        return roles;
-    }
-
-    public Role findByName(ERole name) {
-        for (Role role: roles) {
-            if (role.getName().equals(name)) {
-                return role;
-            }
-        }
-
-        return null;
-    }
 }
